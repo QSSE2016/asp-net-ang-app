79105b309235da17e4f932fa8066d66ae7dbb946
Alexandros <alexstathas1996@gmail.com>
2024-02-27 19:21:53 +0200

    Connected to database via configuration file

--- API (ASP NET Core)/LoginAppAPI/LoginAppAPI/Program.cs
+++ API (ASP NET Core)/LoginAppAPI/LoginAppAPI/Program.cs
@@ -1,3 +1,6 @@
+using LoginAppAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Connect to database
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection"));
+});
 
 var app = builder.Build();
 
