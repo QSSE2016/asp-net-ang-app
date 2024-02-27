946b3d1310c456c0fbe0b42958c52fac89a76928
Alexandros <alexstathas1996@gmail.com>
2024-02-27 19:18:02 +0200

    Added basic outline of web api. Will connect with connection string in next commit.

---
+++ API (ASP NET Core)/LoginAppAPI/LoginAppAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LoginAppAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginAppAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ API (ASP NET Core)/LoginAppAPI/LoginAppAPI/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace LoginAppAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Email { get; set; }
+        public string? Password { get; set; }  
+    }
+}
