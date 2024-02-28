835bb92f7848b7c8eee0c25a3fd1049030624163
Alexandros <alexstathas1996@gmail.com>
2024-02-28 15:31:50 +0200

    some groundwork for the middleman service

--- UI (Angular)/LoginAuthApp/src/app/middleman.service.ts
+++ UI (Angular)/LoginAuthApp/src/app/middleman.service.ts
@@ -2,15 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+// Communicates with the API
+
 @Injectable({
   providedIn: 'root'
 })
 export class MiddlemanService {
 
-  url: string = "http://localhost:4200/api/users"
+  apiUrl: string = "http://localhost:4200/api/"
   constructor(private http: HttpClient) { }
 
-  checkIfUserExists() : Observable<Object> {
-    return this.http.get(this.url)
+  checkIfUserExists(username:string,password:string) : Observable<Object> {
+    return this.http.post(this.apiUrl + "login",{
+        username: username,
+        password: password
+    })
+  }
+
+  addUser(username:string,email:string,password:string) {
+    console.log("creating a new user with email: " + email)
+  }
+
+  deleteUser(username:string) {
+     console.log("deleting user with username: " + username)
   }
 }
